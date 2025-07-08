638365bf2c54344c622ddada7873b26b88ccbeba
Nilesh Sharma <142870089+NileshSharma71@users.noreply.github.com>
2025-07-08 20:26:19 +0530

    Update harden-target.sh

--- harden-target.sh
+++ harden-target.sh
@@ -3,4 +3,4 @@ set -e
 sudo sed -i 's@^[#[:space:]]*PermitRootLogin.*@PermitRootLogin prohibit-password@' /etc/ssh/sshd_config
 sudo sed -i 's@^[#[:space:]]*PasswordAuthentication.*@PasswordAuthentication no@' /etc/ssh/sshd_config
 sudo sed -i 's@^[#[:space:]]*PasswordAuthentication.*@PasswordAuthentication no@' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
-sudo systemctl restart sshd
+sudo systemctl restart ssh
